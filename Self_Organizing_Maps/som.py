160fd75de088331113d0975313918a1b08e5fad0
Bhavyakala <43778437+Bhavyakala@users.noreply.github.com>
2019-08-10 20:03:19 +0530

    self organizing maps

---
+++ Self_Organizing_Maps/som.py
@@ -0,0 +1,44 @@
+# Self Organizing Map
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# importing the dataset 
+dataset = pd.read_csv('Credit_Card_Applications.csv')
+X = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,-1].values
+
+# Feature scaling
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range=(0,1))
+X = sc.fit_transform(X)
+
+#Training the SOM
+from minisom import MiniSom
+som = MiniSom(x = 10, y = 10, input_len = 15, sigma = 1.0, learning_rate = 0.5)
+som.random_weights_init(X)
+som.train_random(X,num_iteration = 100)
+
+# Visualinsing the SOM
+from pylab import bone, pcolor, colorbar, plot, show 
+bone()
+pcolor(som.distance_map().T)
+colorbar()
+markers = ['o', 's']
+colors = ['r','g']
+for i,x in enumerate(X):
+    w = som.winner(x)
+    plot(w[0]+0.5, w[1] + 0.5, markers[y[i]], markeredgecolor = colors[y[i]],
+         markerfacecolor=  'None', markersize = 10, markeredgewidth = 2 )
+show()    
+
+
+#Finding the frauds
+mappings = som.win_map(X) #returns dictionary of customers and winning nodes
+frauds = mappings[(1,2)]
+frauds = sc.inverse_transform(frauds)
+
+
+
